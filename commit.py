aca861f09e1e2ed409b240891af4d34a9c2f5264
Josef Kral <kraljo@oc0653706537.ibm.com>
2012-11-19 08:55:44 +0100

    DOC: commit.py -- next re/birth

---
+++ commit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# for correct echoing needs $> reset
+# for named sockets use script-name: dcomint
+
+import os, pexpect, sys
+l = ' '.join (sys.argv [2:])
+dtach='dtach -A %s/tmp/\%s.dtach -z bash' % (os.environ['HOME'], sys.argv [1])
+
+#(d) print "% -pcomint: " + dtach
+p = pexpect.spawn (dtach)
+
+#>? p.sendline ('')
+#>? p.sendline ('echo ' + l) #>? "% #" 
+print "\n%pcomint> " + l
+p.sendline ('')
+p.sendline (l)
+p.sendline ('')
