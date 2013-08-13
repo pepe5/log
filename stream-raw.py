99145f9f8252f3b08f3c640855b11f2c7c0031e5
Josef Kral <kraljo@oc0653706537.ibm.com>
2013-08-13 18:31:04 +0200

    proc completed

--- stream-raw.py
+++ stream-raw.py
@@ -4,34 +4,40 @@
 #	http://stackoverflow.com/questions/8980050/persistent-python-subprocess
 
 ## usage:
-#$ date -Is | cut -d+ -f1 > 123.out
-#$ S=123.out; tail -f $S | while read L; do echo `date -Is | cut -d+ -f1`: -$S: $L; done &
-#$ dtach -n 123.dtach -z script -f 123.in
-#$ python -u ~/bin/stream-raw.py 123.in 123.out
-#$ dtach -a 123.dtach #$ cat >> 123.helper
-#
-#>? tail -f 123.in | python ~/bin/stream-raw.py 123.out
+#$ T=$(basename `pwd`); H1=$host1..; ...
+#$ date -Is | cut -d+ -f1 > $H1.out
+#$ tail -f $H1.out | while read L; do echo `date -Is | cut -d+ -f1`: -$H1: $L; done &
+#$ dtach -n $T.dtach -z script -f $T.in
+#$ > $T.in; python -u ~/bin/stream-raw.py $T.in $H1.dtach
+
+#$ dtach -a $H1.dtach
+#$ script -af $H1.out
+
+#$ dtach -a $T.dtach
+#$ cat >> $T.hist
 
 import os, pexpect, shlex, subprocess, sys, time
-tail = 'tail -f %s' % sys.argv [1] #>? (!) >> stdin
-script = 'script -af /tmp/%s' % sys.argv [2]
-print '%pcomint:> tail: ' + repr (shlex.split (tail))
+inputter = 'tail -f %s' % sys.argv [1] #>? (!) >> stdin
+outputter = 'dtach -A %s -z bash' % sys.argv [2]
+#>! make dtach-startup starting $> script -af %s
+
+print '%pcomint:> outputter: ' + repr (shlex.split (outputter))
 #>? newin = os.fdopen (sys.stdin.fileno (), 'r', 0)
 #	(vv) stdin = newin,
 
-srcp = subprocess.Popen \
-    (shlex.split (tail),
+inp = subprocess.Popen \
+    (shlex.split (inputter),
      bufsize = 0,
      stdout = subprocess.PIPE)
-dstp = pexpect.spawn (script)
+outp = pexpect.spawn (outputter)
 
 while 1:
-    data = srcp.stdout.read (1)
+    data = inp.stdout.read (1)
     if data == '\x00':
         print '!' + data,
-        data = srcp.stdout.readline () #! fixes dstp.send (data) :-S
+        data = inp.stdout.readline () #! fixes outp.send (data) :-S
     print '.' + data, # '\n%pcomint$> ' #(>) repr (data)
-    dstp.send (data)
+    outp.send (data)
 
 ## OK:
 # process = subprocess.Popen (['tail','-f', '/tmp/1.txt'], bufsize=1, stdout=subprocess.PIPE)
