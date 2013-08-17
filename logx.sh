127ff6c370060bbc56930980f87d4e22c1d6bf57
Josef Kral <kraljo@oc0653706537.ibm.com>
2013-08-17 22:19:33 +0200

    nok: logx .stream.dtach has troubles

--- logx.sh
+++ logx.sh
@@ -4,9 +4,18 @@ H=$1
 T=$(basename `pwd`);
 echo -$T:$H
 
-> $T.in; dtach -n $H.stream.dtach -z python -u stream-raw.py $T.in $H.dtach
+if ! [ -f $T.dtach ]; then
+    dtach -n $T.dtach -z script -f $T.in; chmod -v 600 $T.in
+    sleep 1; commit.py $T "reset; cat >> $T.hist"
+    #>? sleep 1; commit.py $T script -af $T.in
+fi
+
+> $T.in; dtach -n $H.stream.dtach -z stream-raw.py $T.in $H.dtach
 sleep 1; commit.py $H script -af $H.out.log
 sleep 1; xterm -xrm XTerm*ScrollBar:false -T $T:$H -e dtach -a $H.dtach -z &
 sleep 1; commit.py $H ". lk; slsssh root@$H"
 
-#>? nohup
+#() nohup xterm -T $T -e dtach -a $T.dtach -z &
+#() echo stream..py: -timestamp: `date -Is | cut -d+ -f1` > $H.out.log
+#|| tail -f $H.out.log | while read L; do echo `date -Is | cut -d+ -f1`: -$H: $L; done &
+#() dtach -a $H.dtach
